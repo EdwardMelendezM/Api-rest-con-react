bc271714014641eb6f79ef47bb19588c10d411de
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-01-30 16:03:48 -0500

    Subiendo proyecto

---
+++ src/components/CrudApi.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { helpHttp } from '../helpers/helpHttp';
+const CrudApi = () => {
+
+    const [db, setDb] = useState([]);
+    const [dataEdit, setDataEdit] = useState(null);
+    let api = helpHttp();
+    let url="";
+
+    const createData = () => {
+        dataEdit.id = Data.now();
+        setDb([...db, data])
+    }
+    const updateData = (data) => {
+        let newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData)
+    }
+    const deleteData = (data) => {
+        //let newData = db.map
+        console.log("Raa")
+    }
+
+    return (
+        <div>
+            <h2>Crud API</h2>
+            <hr />
+        </div>
+        )
+}
+export default CrudApi;
---
+++ src/helpers/helpHttp.js
@@ -0,0 +1,44 @@
+export const helpHttp = () => {
+    const customFecth = (endpoint, options) => {
+        const defaultHeader = {
+            accept: "application/json"
+        };
+        const controller = new AbortController();
+        options.signal = controller.signal;
+        options.method = options.method || "GET";
+        options.headers = options.headers ? { ...defaultHeader, ...options.headers } : defaultHeader;
+        options.body = JSON.stringify(options.body) || false;
+        if (!options.body) delete options.body;
+
+        console.log(options);
+        setTimeout(() => {
+            controller.abort();
+        }, 3000);
+
+
+        return fecth(endpoint, options)
+            .then(res => res.ok ? res.json() : Promise.reject({
+                err: true,
+                status: res.status || "00",
+                statusText: res.status.Text || "Ocurrio el error"
+            }))
+            .catch(err => err)
+    }
+    const get = (url, options = {}) => customFecth(url, options)
+    const post = (url, options = {}) => {
+        options.method = "POST"
+        return customFecth(url, options)
+    }
+    const put = (url, options = {}) => {
+        options.method = "PUT"
+        return customFecth(url, options)
+    }
+    const del = (url, options = {}) => {
+        options.method = "DELETE"
+        return customFecth(url, options)
+    }
+
+    return {
+        get, post, put, del
+    }
+}
