0aa5dd7fcb3643d7b9507d1a6c8b0280ac85b4a0
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-02 09:39:57 -0500

    Poniendo las funciones y colocando las APIS en funcion de estado

--- src/components/SelectsAninados.jsx
+++ src/components/SelectsAninados.jsx
@@ -5,27 +5,28 @@ const SelectsAninados = () => {
   const [state, setState] = useState("");
   const [town, setTown] = useState("");
   const [suburb, setSuburb] = useState("");
+  const TOKEN = "d81a7ac7-976d-4e1e-b7d3-b1979d791b6c";
   return (
     <div>
       <h2>Selects anidados</h2>
       <h3>Mexico</h3>
       <SelectsList
         title="estados"
-        url=""
+        url={`https://api.copomex.com/query/get_estados?token=${TOKEN}`}
         handleChange={(e) => {
           setState(e.target.value);
         }}
       />
       <SelectsList
         title="municipios"
-        url=""
+        url={`https://api.copomex.com/query/get_municipio_por_estado/Aguascalientes?token=${TOKEN}`}
         handleChange={(e) => {
           setTown(e.target.value);
         }}
       />
       <SelectsList
-        title="colonias"
-        url=""
+        title="colonia"
+        url={`https://api.copomex.com/query/get_colonia_por_municipio/Xochimilco?token=${TOKEN}`}
         handleChange={(e) => {
           setSuburb(e.target.value);
         }}
